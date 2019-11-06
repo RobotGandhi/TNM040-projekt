bdacb1d226c7fecfa1b9d822b98c10c4a9de0e9a
Rubenbromee <45816070+Rubenbromee@users.noreply.github.com>
2019-11-06 12:46:39 +0100

    Update App.js

--- projekt/src/App.js
+++ projekt/src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React Jeff
         </a>
       </header>
     </div>
