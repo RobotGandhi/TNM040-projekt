efb71be032e2f8b29db15364c5f022f0eaf9c92d
CrispyFruits <hugodahl99@hotmail.com>
2019-11-06 13:53:52 +0100

    test branch

--- projekt/src/App.js
+++ projekt/src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React AAA
+          Learn React ASS
         </a>
       </header>
     </div>
