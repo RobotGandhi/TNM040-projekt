eddd0cbcee4279644256b8a4daa5d2dc1e30f215
CrispyFruits <hugodahl99@hotmail.com>
2019-11-06 15:56:18 +0100

    Navbar start

--- projekt/src/App.js
+++ projekt/src/App.js
@@ -1,25 +1,77 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
+
+function Green(props) {
+  return(
+    <div className ="green">
+    G
+    </div>
+  );
+}
+
+function Red(props) {
+  return(
+    <div className ="red">
+    R
+    </div>
+  );
+}
+
+function Blue(props) {
+  return(
+    <div className ="blue">
+    B
+    </div>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React ASS
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Switch> 
+          <Route path="/green">
+            <Green/>
+          </Route>
+          <Route path="/red">
+            <Red/>
+          </Route>
+          <Route path="/blue">
+            <Blue/>
+          </Route>
+          <Route path="/">
+            <div className ="content"></div>
+          </Route>
+        </Switch>
+
+        {/*Navbar*/}
+        <div className="navBar">
+
+          {/*Recipes*/}
+          <Link to ="/green" className="navButton">
+            <div>
+              G
+            </div>
+          </Link>
+          {/*Quick Convert*/}
+
+          <Link to ="/red" className="navButton">
+            <div>
+              R
+            </div>
+          </Link>
+
+          <Link to ="/blue" className="navButton">
+          {/*New recipes*/}
+            <div>
+              B
+            </div>
+          </Link>
+        </div>
+      </div>  
+    </Router>
   );
 }
 
