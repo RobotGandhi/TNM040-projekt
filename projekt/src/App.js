bbfbe054e29b7501f0580e391e9cfe4b7ff20438
Hawkan95 <f.hakansson@hotmail.com>
2019-11-06 13:57:44 +0100

    Bobgogogog

--- projekt/src/App.js
+++ projekt/src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Jeff
+          Bogogobogogo
         </a>
       </header>
     </div>
