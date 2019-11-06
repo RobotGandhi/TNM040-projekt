b0d25a5926fe18ad97814a86496d9a7d3d7d2ec9
Linus <itslinnet@gmail.com>
2019-11-06 13:51:04 +0100

    test2

--- projekt/src/App.js
+++ projekt/src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React BAA
+          Learn React AAA
         </a>
       </header>
     </div>
