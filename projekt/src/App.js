e95c0b46207beea1f4ac775852774e79e15b35d6
Elpern <luckejon@live.com>
2019-11-06 17:00:54 +0100

    ehm

--- projekt/src/App.js
+++ projekt/src/App.js
@@ -1,17 +1,103 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
 import './App.css';
 
+let a = null;
+let b = null;
+
 function App() {
+
+
   return (
     <div className="App">
-    
-   
- <a>
-          Lucas commit1
-        </a>
+      <Router>
+        <Switch>
+        
+          <Route path="/steptwo"><NewRecipeStep2/> </Route>
+          <Route path="/"><NewRecipeStep1/>
+          </Route>
+        </Switch>
+      </Router>
+
     </div>
+
   );
+}
+function NewRecipeStep2(){
+  
+  return(
+    <div className = "main">
+      <Textfieldrecipe/>
+      <Ingredient/>
+      <Ingredient/>
+      <Ingredient/>
+      <Ingredient/>
+
+      <div className = "knapp"> Save </div>
+      <div className = "knapp"> Add </div>
+    </div>
+    );
+}
+function NewRecipeStep1(){
+
+  return(
+
+    <div>
+    <div className = "main">
+<Textfieldrecipe/>
+<p> From </p> <div className = "knapp"> System </div>
+   <p> To </p> <div className = "knapp"> System </div>
+   <Link to="/steptwo"><div className = "knapp">   Create </div></Link>   </div>
+
+</div>
+    )
+}
+
+
+function Ingredient(){
+  return(
+    <div className="ingredientBox">
+    <Textfieldingredient/>
+    <div className = "knapp"> System </div>
+    <div className = "knapp"> System </div>
+    </div>
+    )
+}
+
+
+
+
+function Textfieldingredient() {
+if(b == null){
+    b = "Name of ingredient";
+}
+return(
+<input type="text" id="namnge" placeholder={b} onChange={changeInput2} />
+)
+}
+function Textfieldrecipe() {
+if(a == null){
+    a = "Name of recipe";
+}
+
+
+
+  return(
+    
+   <input type="text" id="namnge" placeholder={a} onChange={changeInput} />
+   
+ 
+    )
+}
+
+function changeInput(event){
+a = event.target.value;
+
+}
+function changeInput2(event){
+b = event.target.value;
+
 }
 
 export default App;
