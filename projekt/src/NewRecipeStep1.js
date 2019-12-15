524d45ac184664c9370553cc90a8576fa4922411
Linus <itslinnet@gmail.com>
2019-12-15 18:17:17 +0100

    CSS New Recipe Step 1

--- projekt/src/NewRecipeStep1.js
+++ projekt/src/NewRecipeStep1.js
@@ -3,58 +3,64 @@ import { BrowserRouter as Router, Switch, Route, Link, useParams } from "react-r
 
 
 const NewRecipeStep1 = (props) => {
- 
-    let convert = require('convert-units');
-  
-    const [recipeName, setRecipeName] = useState("Recipe");
-    const [convertFrom, setConvertFrom] = useState("US-Custom");
-    const [convertTo, setConvertTo] = useState("Metric");
-    const [recipeID, setRecipeID] = useState(props.data.length + 1);
-  
-    function changeRecipeName(event) {
-      setRecipeName(event.target.value);
-    }
-    function changeConvertFrom(event) {
-      setConvertFrom(event.target.value);
-    }
-    function changeConvertTo(event) {
-      setConvertTo(event.target.value);
-    }
 
-    function changeConvertTo(event) {
-      setConvertTo(event.target.value);
-    }
+  let convert = require('convert-units');
 
-    function sendTempValues() {
-        props.callback([recipeName, convertFrom, convertTo, recipeID]);
-    }
+  const [recipeName, setRecipeName] = useState("Recipe");
+  const [convertFrom, setConvertFrom] = useState("US-Custom");
+  const [convertTo, setConvertTo] = useState("Metric");
+  const [recipeID, setRecipeID] = useState(props.data.length + 1);
 
-    return (
-      <div>
-        <div className="main">
-          <input type="text" className="recipeName" placeholder="Name of recipe..." onChange={changeRecipeName}>
-          </input>
-          <p>From</p>
-          <select value={convertFrom} className="button" onChange={changeConvertFrom}>
-            <option value="US-Custom">US-Custom</option>
-            <option value="Metric">Metric</option>
-          </select>
-  
-          <p>To</p>
-          <select value={convertTo} className="button" onChange={changeConvertTo}>
-            <option value="Metric">Metric</option>
-            <option value="US-Custom">US-Custom</option>
-          </select>
-  
-          <Link to={"/steptwo/" + convertFrom + "/" + convertTo + "/" + recipeName}>
-            <div className="button" onClick={sendTempValues}>
-              Create
+  function changeRecipeName(event) {
+    setRecipeName(event.target.value);
+  }
+  function changeConvertFrom(event) {
+    setConvertFrom(event.target.value);
+  }
+  function changeConvertTo(event) {
+    setConvertTo(event.target.value);
+  }
+
+  function changeConvertTo(event) {
+    setConvertTo(event.target.value);
+  }
+
+  function sendTempValues() {
+    props.callback([recipeName, convertFrom, convertTo, recipeID]);
+  }
+
+  return (
+    <div>
+      <h1 className="header">New Recipe</h1>
+      <div className="box">
+        <div className="box-in-box">
+          <div className="newRecipe1">
+            <h2 className="newRecipeHeader">Convert:</h2>
+            <input type="text" className="recipeName" placeholder="Name of recipe..." onChange={changeRecipeName}>
+            </input>
+            <div>
+              <h2 className="fromTo">From</h2>
+              <select value={convertFrom} className="dropdown2" onChange={changeConvertFrom}>
+                <option value="US-Custom">US-Custom</option>
+                <option value="Metric">Metric</option>
+              </select>
             </div>
+            <div>
+              <h2 className="fromTo">To</h2>
+              <select value={convertTo} className="dropdown2" onChange={changeConvertTo}>
+                <option value="Metric">Metric</option>
+                <option value="US-Custom">US-Custom</option>
+              </select>
+            </div>
+          </div>
+          <Link to={"/steptwo/" + convertFrom + "/" + convertTo + "/" + recipeName}>
+            <div className="convertButton" onClick={sendTempValues}>Create!</div>
           </Link>
         </div>
       </div>
-    );
-  
+    </div>
+  );
+
 }
 
 
