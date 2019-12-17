240be873760a4685572a2268f50a9a3919a64119
Linus <itslinnet@gmail.com>
2019-12-17 11:47:46 +0100

    Merge Error delux modal work

--- projekt/src/SavedRecipes.js
+++ projekt/src/SavedRecipes.js
@@ -13,70 +13,92 @@ const ListOfRecipes = (props) => {
   function afterOpenModal() {
     //References are synced and can be accesssed here.
   }
-  function closeModal() {
+  function closeModal(e) {
+    e.stopPropagation(); 
     setIsOpen(false);
   }
 
   return (
     <div>
       <h1 className="header">Book of Recipes</h1>
-      {props.data.map(recipe => 
-      <div className ="recipeBlock">
-        <div className = "recipeElement">
-          <button id={recipe.id} onClick={openModal}>
-            {recipe.name}
-          </button>
-          <Modal 
-          isOpen={modalIsOpen}
-          onAfterOpen={afterOpenModal}
-          onRequestClose={closeModal}
-          data-target={recipe.id}
-          data={recipe}
-          >
-            <div>
-              {props.data.filter(r => r.id === recipe.id).map(recipe =>
+      {props.data.map(recipe =>
+        <div className="recipeBlock">
+          <div className="recipeElement" onClick={openModal}>
+            <h1 id={recipe.id}>{recipe.name}</h1>
+            <Modal className="descriptionModal"
+              isOpen={modalIsOpen}
+              onAfterOpen={afterOpenModal}
+              onRequestClose={closeModal}
+              data-target={recipe.id}
+              data={recipe}
+            >
+              <div>
+                {props.data.filter(r => r.id === recipe.id).map(recipe =>
                   <div>
-                      <h1>{recipe.name}</h1>
-                      <ul>
-                          {recipe.ingredients.map( ingredient =>
-                              <div>
-                                  <h2>Ingredients: </h2>
-                                  <span>{ingredient.conversionResult} </span>
-                                  <span>{ingredient.ingredientConvertTo} </span>
-                                  <span>{ingredient.ingredientName} </span>
-                             </div>
-                         )}
-                      </ul>
-                      <h2>Description: </h2>
+                    <h2 className="recipeTitle">{recipe.name}</h2>
+                    <div className="bookOfRecipesDescription">
+                      <h2>Ingredients: </h2>
+                      {recipe.ingredients.map(ingredient =>
+                        <div>
+                          {ingredient.conversionResult}
+                          {ingredient.ingredientConvertTo}
+                          {ingredient.ingredientName}
+                        </div>
+                      )}
+                      <h2>Description:</h2>
                       <div>
                         {recipe.description}
                       </div>
-                </div>
-              )}
-            </div>
-          </Modal>
-        </div>
-          <div className = "deleteButton" id={recipe.id} onClick={deleteRecipe}>
+                    </div>
+                  </div>
+                )}
+                <div onClick={closeModal} className="button" >Back</div>
+              </div>
+            </Modal>
+          </div>
+          <div className="deleteButton" id={recipe.id} onClick={deleteRecipe}>
             x
           </div>
-      </div>
+        </div>
       )}
 
       {/*TEMPORARY FOR EASY STYLING*/}
-        <div className = "recipeBlock">
-          <div className = "recipeElement">
-            <Link to="">
-              <h1>Recipe name</h1>
-            </Link>
-          </div>
-          <div className = "deleteButton">
-            x
-          </div>
+      {/* 
+      <div className="recipeBlock">
+        <div className="recipeElement" onClick={openModal}>
+          <h1>Recipe name</h1>
         </div>
+        <div className="deleteButton">
+          x
+          </div>
+        <Modal className="descriptionModal"
+          isOpen={modalIsOpen}
+          onAfterOpen={afterOpenModal}
+          onRequestClose={closeModal}
+          ariaHideApp={false}
+          contentLabel="Example Modal">
+          <div>
+            <h3>NAME</h3>
+            <div className = "bookOfRecipesDescription">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vulputate libero sit amet elit vestibulum ultricies. Nunc congue commodo ante, a porta tortor rhoncus vel. Ut tellus justo, sagittis in varius eu, aliquam quis urna. Nulla ornare lorem quis urna ullamcorper, vitae dapibus tortor ultrices. Nulla non ex vitae mi tristique ornare. Integer nec tempus urna. Aliquam ut lacinia nisi. Proin at blandit dolor, sit amet faucibus quam. Praesent aliquam libero eu maximus dapibus. Etiam eleifend enim ut efficitur imperdiet.
+
+Maecenas dapibus id enim at tristique. Vestibulum pharetra tellus non semper iaculis. Morbi mattis urna sit amet ultrices ullamcorper. Curabitur commodo urna quam, non molestie lectus finibus vitae. Pellentesque eleifend massa eget sapien tincidunt, in dictum augue ultrices. Sed gravida purus elit. Aliquam ornare nisi odio, id vehicula nulla euismod eu. Nulla malesuada magna vitae convallis dapibus. In erat libero, faucibus id pretium ut, ultrices rhoncus tellus. Integer mauris justo, elementum quis efficitur nec, interdum sed augue.
+
+Donec iaculis diam libero, nec tempus felis lacinia et. Donec id justo eget nunc elementum dignissim. In id luctus mi, non vulputate urna. Sed et mi ac magna rutrum laoreet. Nullam sit amet orci magna. Aenean vel aliquet massa. Ut nec congue sapien. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
+
+Donec blandit erat et tortor volutpat, vel blandit massa auctor. Aenean lacus mi, elementum vitae felis at, lobortis rutrum risus. Curabitur imperdiet libero purus, ac egestas tortor posuere quis. Mauris luctus, urna non aliquam gravida, nulla ipsum pretium erat, id rhoncus augue orci sit amet mi. Morbi quis elementum dui, a feugiat nunc. Etiam nec malesuada turpis. Curabitur scelerisque dui quis facilisis semper. Aliquam interdum congue egestas. Proin maximus felis vehicula, dignissim leo ac, lobortis mauris. Nulla placerat pulvinar ex, ut sagittis arcu venenatis vel. Quisque lobortis et ipsum non imperdiet. Quisque at lacus sed mauris pharetra pellentesque. Donec quis justo vehicula urna sagittis tincidunt. Phasellus facilisis tellus at ipsum placerat placerat.
+
+Vestibulum eu est purus. Nullam condimentum dui scelerisque lacus congue, et malesuada odio fringilla. Donec suscipit magna ac ligula pulvinar, ut commodo tortor rhoncus. Duis dictum malesuada quam, eget lacinia ligula lacinia ullamcorper. Sed consectetur leo urna, vitae ullamcorper ante rutrum venenatis. In eu nibh molestie, pretium tortor eu, eleifend purus. Phasellus at blandit dolor. Maecenas auctor nisi id aliquam bibendum.
+            </div>
+            <div onClick={closeModal} className="button" >Back</div>
+          </div>
+        </Modal>
+      </div>
+        */}
       {/*TEMPORARY FOR EASY STYLING*/}
     </div>
   )
-  
+
   // Creates a temporary array that filters out the recipe you deleted and sets the recipe array to the temporary array
   function deleteRecipe(event) {
     const temp = props.data.filter(r => r.id != event.target.id);
@@ -84,4 +106,4 @@ const ListOfRecipes = (props) => {
   }
 }
 
-    export default ListOfRecipes;
+export default ListOfRecipes;
